91c214d168ae6ec3aaac46f4111f942d7059750f
aa <nguyenquickd@gmail.com>
2021-08-07 01:44:41 +0700

    sap

--- server/index.js
+++ server/index.js
@@ -24,12 +24,12 @@ connectDB()
     
 const app = express()
 app.use(express.json())
-//app.get('/', (req, res) => res.send('Hello world'))
 app.use(cors())
+
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server start on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
