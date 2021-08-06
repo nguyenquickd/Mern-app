d56c6c59743821594a9efdfc6946cf9debf0d574
aa <nguyenquickd@gmail.com>
2021-08-07 01:48:00 +0700

    sapfff

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,102 +1,72 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const argon2 = require('argon2')
-const User = require('../models/User')
 const jwt = require('jsonwebtoken')
-const verifyToken = require('../middleware/auth') 
+const verifyToken = require('../middleware/auth')
 
-router.get('/', verifyToken, async(req,res) => {
+const User = require('../models/User')
+
+// @route GET api/auth
+// @desc Check if user is logged in
+// @access Public
+router.get('/', verifyToken, async (req, res) => {
 	try {
 		const user = await User.findById(req.userId).select('-password')
-		if(!user) return res.status(400).json({success: false, message: 'User not found'})
-		res.json({success: true, user})
+		if (!user)
+			return res.status(400).json({ success: false, message: 'User not found' })
+		res.json({ success: true, user })
 	} catch (error) {
 		console.log(error)
-		res.status(500).json({success: false, message: 'Internal server Error'})
+		res.status(500).json({ success: false, message: 'Internal server error' })
 	}
 })
+
 // @route POST api/auth/register
 // @desc Register user
 // @access Public
+router.post('/register', async (req, res) => {
+	const { username, password } = req.body
 
-router.post('/register', async(req, res) => {
-    const {username, password} = req.body
+	// Simple validation
+	if (!username || !password)
+		return res
+			.status(400)
+			.json({ success: false, message: 'Missing username and/or password' })
 
-    //simple validate
-    if(!username || !password){
-        return res.status(400).json({success: false, message: 'Missing UserName or Password'})
-    }
+	try {
+		// Check for existing user
+		const user = await User.findOne({ username })
 
-    try{
-        //Check for existing user
-        const user = await User.findOne({username: username})
-        if(user){
-            return res.status(400).json({success: false, message: 'Username is already registered'})
-        }
+		if (user)
+			return res
+				.status(400)
+				.json({ success: false, message: 'Username already taken' })
 
-        // ALL GOOD
-        const hashedPassword = await argon2.hash(password)
-        const newUser = new User({username,password: hashedPassword})
-        await newUser.save()
+		// All good
+		const hashedPassword = await argon2.hash(password)
+		const newUser = new User({ username, password: hashedPassword })
+		await newUser.save()
 
-        // return Token
-        const accessToken = jwt.sign(
-            {userId: newUser._id},
-            process.env.ACCESS_TOKEN_SECRET
-        )
+		// Return token
+		const accessToken = jwt.sign(
+			{ userId: newUser._id },
+			process.env.ACCESS_TOKEN_SECRET
+		)
 
-        res.json({success: true, message: 'You got the token', accessToken})
-    }catch(error){
-        console.log(error)
-        res.status(500).json({success: false, message: 'Internal server Error'})
-    }
+		res.json({
+			success: true,
+			message: 'User created successfully',
+			accessToken
+		})
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+	}
 })
 
 // @route POST api/auth/login
 // @desc Login user
 // @access Public
-
-// router.post('/login', async(req,res) => {
-//     const {username, password} = req.body
-   
-//     //simple validate
-//     if(!username || !password){
-//         return res
-//         .status(400)
-//         .json({success: false, message: 'Missing UserName or Password'})
-//     }
-
-//     try{
-//         //check for existing user
-//         const user = await User.findOne({username})
-//         if(!user){
-//             return res.status(400).json({success: false, message: 'Incorrect UserName or Password'})
-//         }
-
-//         // Username found
-//         const passwordValid = await argon2.verify(user.password, password)
-//         if(!passwordValid){
-//             return res.status(400).json({success: false, message: 'Incorrect UserName or Password'})
-//         }
-
-//         //All good
-//         // return Token
-//         const accessToken = jwt.sign(
-//             {userId: User._id},
-//             process.env.ACCESS_TOKEN_SECRET
-//         )
-
-//         res.json({
-//             success: true,
-//             message: 'Logged in successfully',
-//             accessToken
-//         })
-//     }catch(error){
-//         console.log(error)
-//         res.status(500).json({success: false, message: 'Internal server Error'})
-//     }
-// } )
-
 router.post('/login', async (req, res) => {
 	const { username, password } = req.body
 
